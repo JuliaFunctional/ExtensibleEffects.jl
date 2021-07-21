049d239b3a7528b7794ccf238c984cdc218ad338
Stephan Sahm <stephan.sahm@gmx.de>
2021-07-21 17:51:42 +0200

    adding doctests to test suite for better test coverage

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,9 +2,17 @@ using Test
 using ExtensibleEffects
 using DataTypesBasic
 using TypeClasses
+using Documenter
+
 splitln(str) = split(strip(str), "\n")
 import ExtensibleEffects: autorun
 
+@test isempty(detect_ambiguities(ExtensibleEffects))
+
+if  v"1.6" <= VERSION < v"1.7"
+  # doctests are super instable, hence we only do it for a specific Julia Version
+  doctest(ExtensibleEffects)
+end
 
 @testset "Identity/Nothing/Const" begin
   include("OptionTryEither.jl")
